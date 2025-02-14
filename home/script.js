b92ed4f7bf398fc12d32776677b608cdc54cb3d0
Janice Maxene Salipande <jdsalipande2@student.apc.edu.ph>
2025-02-14 14:06:00 +0800

    Update script.js

--- home/script.js
+++ home/script.js
@@ -3,6 +3,9 @@ const SUPABASE_URL = 'https://xnknxmmlfphomxixfgol.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inhua254bW1sZnBob214aXhmZ29sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk1MDkyMDIsImV4cCI6MjA1NTA4NTIwMn0.-iZuTbbKblvRpc3BWg07zSKyjPTA-O8n-Ql0uHPuckI';
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
+console.log("Connecting to Supabase...");
+console.log(supabase);
+
 async function fetchCategories() {
     let { data, error } = await supabase.from('category').select('*');
     
